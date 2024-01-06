82f80b14b230abdf458e2ba47398afedb353ea49
JaimeYuY <y15755141030@163.com>
2024-01-06 17:08:11 +0800

    描述：代码风格优化

--- src/main/java/com/ocean/angel/tool/enums/ExportTypeEnum.java
+++ src/main/java/com/ocean/angel/tool/enums/ExportTypeEnum.java
@@ -7,11 +7,15 @@ package com.ocean.angel.tool.enums;
  * @time: 2023/5/20 16:25
  */
 public enum ExportTypeEnum {
+
     ALL(0, "导出所有的表"),
+
     NEEDED(1, "只导出配置的表"),
+
     UNNEEDED(2, "导出除了配置之外的表");
 
     private String msg;
+
     private Integer code;
 
     ExportTypeEnum(Integer code, String msg) {
